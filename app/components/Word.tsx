f619b29c5fa5974844cd937f5df1a7b4b61f1acc
lemmoor <49235295+lemmoor@users.noreply.github.com>
2024-06-02 15:56:55 +0200

    fix styling

--- app/components/Word.tsx
+++ app/components/Word.tsx
@@ -5,7 +5,7 @@ interface WordProps {
 
 export default function Word({ word, previousLetters }: WordProps) {
   return (
-    <div className='w-full p-16 font-mono font-black text-3xl lg:text-8xl text-center'>
+    <div className='w-full sm:p-16 font-mono font-black text-3xl lg:text-8xl text-center'>
       {word.split('').map((letter, i) => {
         if (previousLetters.includes(letter.toUpperCase())) {
           return `${letter} `;
